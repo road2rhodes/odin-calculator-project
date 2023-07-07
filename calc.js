31ff050166a0d09c98f46f2ef2296ae3a7e29520
road2rhodes <132181574+road2rhodes@users.noreply.github.com>
2023-07-07 18:12:03 -0500

    Boilerplate and pseudocde

---
+++ calc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/*
+
+* Calculator must add, subtract, multiply, divide
+* Operations will be an operand, an operator, and then another operand.
+*** Each item must be stored in separate variable (num1, opr, num2)
+
+*** For design, calculator should have buttons for functions, equal, and clear.
+
+* Create function that routes input. It will input first number and then check for value of operator variable to plug in 2nd number.
+
+*/
