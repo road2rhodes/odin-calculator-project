21218428bae2a7545970b0438c5cf9ce4fa5d5e7
road2rhodes <132181574+road2rhodes@users.noreply.github.com>
2023-07-26 17:52:05 -0500

    Remove commented out code

--- calc.js
+++ calc.js
@@ -1,26 +1,5 @@
 'use strict';
 
-/*
-
-* Calculator must add, subtract, multiply, divide
-* Operations will be an operand, an operator, and then another operand.
-*** Each item must be stored in separate variable (num1, opr, num2)
-** Equation will show on display as string. Once equal is hit, a function will evaluate the express and choose the correct computation, based on the value of operator. For example if operator is 'x', calculator will know to multiply.
-
-** All will have default value and when clear will be '' to make falsy. Num 2 will have check to make sure value is not falsy.
-
-** Will create four separation functions for the equations
-
-** All numbers will be rounded to avoid floats in first version of calculator.
-
-** Calculator might have memory. Results will be story in an array. Up to five numbers. If length === 5, it will push in latest result and shift out the earliest one.
-
-*** For design, calculator should have buttons for functions, equal, and clear.
-
-* Create function that routes input. It will input first number and then check for value of operator variable to plug in 2nd number.
-
-*/
-
 // Calculator Display/Input Variables
 
 let num1, num2, operator, result;
@@ -69,36 +48,6 @@ const CALCULATOR = {
     displayResultDiv.innerText = result;
 
     return result;
-
-    // switch (operator) {
-
-    //   // operator
-
-    //   case '+': this.result = CALCULATOR.operators.add.function(parseInt(num1),parseInt(num2));
-    //   displayResult.innerText = this.result;
-    //   return this.result
-
-    //   case '-': this.result = CALCULATOR.operators.subtract.function(parseInt(num1),parseInt(num2));
-    //   ;
-    //   displayResult.innerText = this.result;
-    //   return this.result
-
-    //   case 'x': this.result = CALCULATOR.operators.multiply.function(parseInt(num1),parseInt(num2));
-    //   displayResult.innerText = this.result;
-    //   return this.result;
-
-    //   case '÷': this.result = CALCULATOR.operators.divide.function(parseInt(num1),parseInt(num2));;
-    //   displayResult.innerText = this.result;
-    //   return this.result
-
-    //   case '^': this.result = CALCULATOR.operators.exponentiate.function(parseInt(num1),parseInt(num2));
-    //   displayResult.innerText = this.result;
-    //   return this.result
-
-    //   default:
-    //     alert('No bueno');
-      
-    // }
     
   }, 
   displayEquation() {
@@ -122,17 +71,6 @@ const CALCULATOR = {
       }
 
       operator = CALCULATOR.operators[this.value].symbol;
-
-      // if (result) {
-      //   operator = CALCULATOR.operators[this.value].symbol;
-      //   num1 = result; num2 = '';
-      // } else if (num1 && operator && num2 && result) {
-      //   num1 = CALCULATOR.operate();
-      //   num2 = '';
-      //   operator = CALCULATOR.operators[this.value].symbol;
-      // }
-
-      
 
     }
 
